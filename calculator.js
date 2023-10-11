e429c8af69b63be7823282a3f47607c6ec2d3ca7
Ndzi Nfor <ndzinfor@gmail.com>
2023-10-11 10:14:59 +0100

    create operate function to receive 2 operands and an operator

--- calculator.js
+++ calculator.js
@@ -11,3 +11,19 @@ const divide = function (a,b){
     return a / b;
 };
 
+const operate = function(a,b,c){
+    switch(b){
+        case "+":
+            add(a,c);
+            break;
+        case "-" :
+            subtract(a,c);
+            break;
+        case "*":
+            multiply(a,c);
+            break;
+        case "/":
+            divide(a,c);
+            break;
+    }
+}
