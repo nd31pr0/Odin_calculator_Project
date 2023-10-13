a5d333a2047a539440bd76baf09e26b432509ca4
Ndzi Nfor <ndzinfor@gmail.com>
2023-10-13 12:52:50 +0100

    complete operate function to evaluate displayed expression

--- calculator.js
+++ calculator.js
@@ -11,22 +11,17 @@ const divide = function (a,b){
     return a / b;
 };
 
-const operate = function(a,b,c){
-    switch(b){
-        case "+":
-            add(a,c);
-            break;
-        case "-" :
-            subtract(a,c);
-            break;
-        case "*":
-            multiply(a,c);
-            break;
-        case "/":
-            divide(a,c);
-            break;
-    }
+
+const operate = function (){
+    let x = document.getElementById('result').value
+
+    let y = eval(x);
+
+    document.getElementById('result').value = y
+
+    return y
 }
+
 const updateDisplay = function(val){
     document.getElementById("result").value += val;
     return val;
